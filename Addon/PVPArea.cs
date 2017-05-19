989cfa5afc6ceba99aee76d9c85e303af995dcfc
Faiye <snowfox06@gmail.com>
2017-05-19 15:55:54 -0500

    Update PVPArea.cs

--- Addon/PVPArea.cs
+++ Addon/PVPArea.cs
@@ -9,6 +9,8 @@ public partial class Player
 {
     [Header("Player Vs Player System")]
     [SyncVar_] public int MinLevel= 0;
+    [SyncVar_] public float StartPos = 0;
+    [SyncVar_] public float EndPos = 0;
 
     /*
      * IsAttackable
@@ -27,9 +29,10 @@ public partial class Player
     /*
      * InZoneName_Example
      * You cannot attack a player if in these zones.
+     * (Made Simple)
      */
     public bool InZoneName_Example()
     {
-        return (transform.position.x > 0 && transform.position.x < 25 && transform.position.z > 0 && transform.position.z < 25);
+        return (transform.position.x > StartPos && transform.position.x < EndPos && transform.position.z > StartPos && transform.position.z < EndPos);
     }
 }
